374b436eab8c50e4c7528e64f7b17ad289ea7e1e
Shapoval <olekcandrsapoval41@gmail.com>
2022-11-15 16:08:26 +0200

    Ex_1

---
+++ main.py
@@ -0,0 +1,16 @@
+input = int(input("Номер дня -->"))
+
+if input == 1:
+    print("Понеділок")
+elif input == 2:
+    print("Вівторок")
+elif input == 3:
+    print("Середа")
+elif input == 4:
+    print("Четверг")
+elif input == 5:
+    print("Пятниця")
+elif input == 6:
+    print("Субота")
+elif input == 7:
+    print("Неділя")
