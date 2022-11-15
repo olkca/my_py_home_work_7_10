a144202b57bf23f9f09f0078b3c79da10e0c245e
Shapoval <olekcandrsapoval41@gmail.com>
2022-11-15 20:17:39 +0200

    Ex_4

---
+++ main.py
@@ -0,0 +1,9 @@
+input1 = int(input("Num --> "))
+input2 = int(input("Num --> "))
+
+if input1 > input2:
+    print(input2 , input1)
+elif input1 < input2:
+    print(input1 , input2)
+elif input1 == input2:
+    print(f"{input2} = {input1}")
