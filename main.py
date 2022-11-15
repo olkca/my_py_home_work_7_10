cccbe24209358f131df0f4726386d92e829f724b
Shapoval <olekcandrsapoval41@gmail.com>
2022-11-15 20:09:41 +0200

    Ex_2

---
+++ main.py
@@ -0,0 +1,26 @@
+input = int(input("Номер місяця --> "))
+
+if input == 1:
+    print("Січень")
+elif input == 2:
+    print("Лютий")
+elif input == 3:
+    print("Березень")
+elif input == 4:
+    print("Квітень")
+elif input == 5:
+    print("Травень")
+elif input == 6:
+    print("Червень")
+elif input == 7:
+    print("Липень")
+elif input == 8:
+    print("Серпень")
+elif input == 9:
+    print("Вересень")
+elif input == 10:
+    print("Жовтень")
+elif input == 11:
+    print("Листопад")
+elif input == 12:
+    print("Грудень")
