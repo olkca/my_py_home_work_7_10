c29a3b263eba6b09900597e1a9c1af01de545850
Shapoval <olekcandrsapoval41@gmail.com>
2022-11-15 20:12:40 +0200

    Ex_3

---
+++ main.py
@@ -0,0 +1,8 @@
+input = int(input("Num --> "))
+
+if input > 0:
+    print("Number is positive")
+elif input < 0:
+    print("Number is negative")
+elif input == 0:
+    print("Number is equal to zero")
